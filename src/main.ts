8d930623a4e68206ca94e7b1f6a16fd41d7b0dbc
xyashino <szczypiorpan123@gmail.com>
2023-05-20 13:41:07 +0200

    Add Cors config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { corsConfig } from './config/cors.config';
 async function bootstrap() {
   const { PORT } = process.env;
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsConfig);
   await app.listen(PORT);
 }
 bootstrap();
