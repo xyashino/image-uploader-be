13dcc6048ed23c02e797c5bac515b42579c3e846
xyashino <szczypiorpan123@gmail.com>
2023-05-21 15:16:16 +0200

    Remove unused routes from ImageController

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -3,40 +3,29 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  Inject,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
 
 @Controller('image')
 export class ImageController {
-  constructor(private readonly imageService: ImageService) {}
+  @Inject(ImageService)
+  private readonly imageService: ImageService;
 
   @Post()
   create(@Body() createImageDto: CreateImageDto) {
     return this.imageService.create(createImageDto);
   }
-
-  @Get()
-  findAll() {
-    return this.imageService.findAll();
-  }
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.imageService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.imageService.findOne(id);
   }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateImageDto: UpdateImageDto) {
-    return this.imageService.update(+id, updateImageDto);
-  }
-
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.imageService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.imageService.remove(id);
   }
 }
