c403456bd8ae7ed8af76426dd182bc1d55104827
xyashino <szczypiorpan123@gmail.com>
2023-05-21 14:29:19 +0200

    Create pipes.config and integrate it into main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { corsConfig } from './config/cors.config';
+import { ValidationPipe } from '@nestjs/common';
+import { pipesConfig } from './config/pipes.config';
 async function bootstrap() {
   const { PORT } = process.env;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe(pipesConfig));
   app.enableCors(corsConfig);
   await app.listen(PORT);
 }
