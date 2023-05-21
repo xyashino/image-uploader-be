c47ea0ed8debc79d29d2edcc5b7ba8e2f76b7664
xyashino <szczypiorpan123@gmail.com>
2023-05-21 14:03:14 +0200

    Integrate TypeORM config in app.module.ts for database access

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { UploadModule } from './upload/upload.module';
 import { ConfigModule } from '@nestjs/config';
 import { StartupService } from './startup.service';
+import { typeormConfigAsync } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync(typeormConfigAsync),
     UploadModule,
   ],
   providers: [StartupService],
