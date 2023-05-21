65dc656f10d578b4c408581d72cf9ddb6dcd6a76
xyashino <szczypiorpan123@gmail.com>
2023-05-21 14:25:16 +0200

    Add id and originalName columns to Image entity

--- src/image/entities/image.entity.ts
+++ src/image/entities/image.entity.ts
@@ -1,1 +1,12 @@
-export class Image {}
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Image extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  originalName: string;
+}
