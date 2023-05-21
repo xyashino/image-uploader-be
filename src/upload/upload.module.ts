ee4a8ee644ddc4e96907f5c8cfeb2d3fea495b47
xyashino <szczypiorpan123@gmail.com>
2023-05-21 15:19:33 +0200

    Implement delete and findOne routes logic in ImageService

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
+import { ImageModule } from '../image/image.module';
 @Module({
+  imports: [ImageModule],
   controllers: [UploadController],
   providers: [UploadService],
 })
