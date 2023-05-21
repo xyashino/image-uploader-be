f0f6b9e961de27acce50283196c96b531623f39e
xyashino <szczypiorpan123@gmail.com>
2023-05-21 15:18:36 +0200

    Implement delete and findOne routes logic in ImageService

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
-
+import { Image } from './entities/image.entity';
+import { ConfigService } from '@nestjs/config';
+import { unlink } from 'node:fs/promises';
 @Injectable()
 export class ImageService {
-  create(createImageDto: CreateImageDto) {
-    return 'This action adds a new image';
+  @Inject(ConfigService)
+  private readonly configService: ConfigService;
+  async create({ originalName }: CreateImageDto) {
+    const image = new Image();
+    image.originalName = originalName;
+    const savedImage = await image.save();
+    savedImage.filePath = `/${savedImage.id}/${originalName}`;
+    return savedImage.save();
   }
-
-  findAll() {
-    return `This action returns all image`;
+  async findOne(id: string) {
+    const foundImage = await Image.findOneBy({ id });
+    console.log(foundImage);
+    if (!foundImage)
+      throw new NotFoundException(`Image with #${id} id not found`);
+    return foundImage;
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} image`;
-  }
-
-  update(id: number, updateImageDto: UpdateImageDto) {
-    return `This action updates a #${id} image`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} image`;
+  async remove(id: string) {
+    const foundImage = await this.findOne(id);
+    const uploadDir = this.configService.get('UPLOAD_DIR').replace(/\/$/, '');
+    try {
+      await unlink(`${uploadDir}${foundImage.filePath}`);
+      await foundImage.remove();
+    } catch (error) {
+      throw new BadRequestException(
+        `Failed to delete the file: ${error.message}`,
+      );
+    }
+    return foundImage;
   }
 }
